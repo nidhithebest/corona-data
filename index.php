7a090110d645acf1d7e2693a7f1264d37f812b2d
nidhithebest <8989nidhi@gmail.com>
2020-03-25 18:35:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 
+
+<?php 
+
+try {
+    $restClient = new RestClient();
+    $response   = $restClient->get('https://corona.lmao.ninja/countries/india');
+    $statusCode = $response->getStatusCode();
+    $content    = $response->getContent();
+  echo "Total Cases: "+$content['cases'];
+} catch(OperationTimedOutException $e) {
+    // do something
+}
+
+?>

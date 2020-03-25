3dd3ea091735f6570efa0b99030cb4250f469d07
nidhithebest <8989nidhi@gmail.com>
2020-03-25 19:00:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
        curl_setopt($curl, CURLOPT_POST, false);
        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+echo "dollcy";
        $curl_response = curl_exec($curl);
+echo "dollu";
        curl_close($curl);
      echo "hello dolly"+$curl_response['flag'];  
 ?>

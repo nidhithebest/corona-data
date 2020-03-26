f2811b2beb376a7b0e8d5b2cd405085aa0ecdb54
nidhithebest <8989nidhi@gmail.com>
2020-03-26 09:17:09 +0530

    W

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ curl_close($curl);
 <html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=0" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>CORONA COUNT</title>
 <style>
 img {
